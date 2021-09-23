c2f125fb3c276f52277f721d1c630025cb5bd159
matsouto <matsouto55@gmail.com>
2021-09-23 13:01:28 -0300

    start planner and fix problems

--- app/src/main/java/com/souto/workoutapp/MainActivity.java
+++ app/src/main/java/com/souto/workoutapp/MainActivity.java
@@ -2,15 +2,19 @@ package com.souto.workoutapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import java.util.Objects;
 
 // MENU
 
 public class MainActivity extends AppCompatActivity {
+
+    protected Button btn_planner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +25,21 @@ public class MainActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide(); // Hide Action Bar
 
         setContentView(R.layout.activity_main);
+
+        //----------------------------------------------
+
+        // Setting the callback for the planner button
+        btn_planner = (Button) findViewById(R.id.btn_planner);
+        btn_planner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startPlanner();
+            }
+        });
+    }
+
+    private void startPlanner() {
+        Intent intent_planner = new Intent(this, WorkoutPlanner.class);
+        startActivity(intent_planner);
     }
 }
--- app/src/main/java/com/souto/workoutapp/WorkoutPlanner.java
+++ app/src/main/java/com/souto/workoutapp/WorkoutPlanner.java
@@ -3,12 +3,27 @@ package com.souto.workoutapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class WorkoutPlanner extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_workoutplanner);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION); // Hide Navigation
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN); // Hide Status
+
+        getSupportActionBar().setTitle("P L A N N E R");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setContentView(R.layout.activity_workout_planner);
+
+        //-----------------------------------------------------
+
+
+
     }
 }
