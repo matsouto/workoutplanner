f2f850edc6ce8f203071fde0c8dea41522a0d790
matsouto <matsouto55@gmail.com>
2021-09-24 19:31:23 -0300

    add login files

---
+++ app/src/main/java/com/souto/workoutapp/Login.java
@@ -0,0 +1,20 @@
+package com.souto.workoutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION); // Hide Navigation
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN); // Hide Status
+
+        setContentView(R.layout.activity_login);
+    }
+}
