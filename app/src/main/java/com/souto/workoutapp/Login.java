0dae73eddf8fc2103dc979797e6cc3f281e3bc80
matsouto <matsouto55@gmail.com>
2021-09-24 21:11:14 -0300

    finish login screen

--- app/src/main/java/com/souto/workoutapp/Login.java
+++ app/src/main/java/com/souto/workoutapp/Login.java
@@ -1,21 +1,20 @@
 package com.souto.workoutapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Objects;
@@ -45,30 +44,32 @@ public class Login extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         ckb_show = findViewById(R.id.login_show);
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String getEmail = edt_email.getText().toString();
-                String getPassword = edt_password.getText().toString();
+        btn_login.setOnClickListener(view -> {
+            String getEmail = edt_email.getText().toString();
+            String getPassword = edt_password.getText().toString();
 
-                if(!TextUtils.isEmpty(getEmail) || !TextUtils.isEmpty(getPassword)) {
-                    mAuth.signInWithEmailAndPassword(getEmail,getPassword)
-                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if(task.isSuccessful()) {
-                                        openMainScreen();
-                                    }else{
-                                        String error = task.getException().getMessage();
-                                        Toast.makeText(Login.this, ""+error, Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            });
-                }
+            if(!TextUtils.isEmpty(getEmail) || !TextUtils.isEmpty(getPassword)) {
+                mAuth.signInWithEmailAndPassword(getEmail,getPassword)
+                        .addOnCompleteListener(task -> {
+                            if(task.isSuccessful()) {
+                                openMainScreen();
+                            }else{
+                                String error = Objects.requireNonNull(task.getException()).getMessage();
+                                Toast.makeText(Login.this, ""+error, Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
 
+        ckb_show.setOnCheckedChangeListener((compoundButton, b) -> {
+            if(compoundButton.isChecked()) {
+                edt_password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            }else{
+                edt_password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            }
+        });
     }
+
 
     private void openMainScreen() {
         Intent intent = new Intent(this,MainActivity.class);
